b593658aaf5429c8d2757dd7d2a7784fb714adef
cpm <mirascarlo934@gmail.com>
2022-09-27 20:56:48 +0800

    Add router exercise

--- temp/hello_react/src/App.js
+++ temp/hello_react/src/App.js
@@ -11,25 +11,60 @@ import HigherLowerHooks from "./HigherLowerHooks";
 import ListSample from "./ListSample";
 import TodoBasic from "./TodoBasic";
 
-class App extends Component {
-  render() {
-    return (
-        <React.Fragment>
-          {/*<StateSample/>*/}
-          {/*<InputSample/>*/}
-          {/*<StateCounter/>*/}
-          {/*<HigherLower/>*/}
-          {/*<Converter/>*/}
-          {/*<HookSample/>*/}
-          {/*<StateCounterHooks/>*/}
-          {/*<ConverterHooks/>*/}
-          {/*<HigherLower/>*/}
-          {/*<HigherLowerHooks/>*/}
-          {/*<ListSample/>*/}
-          <TodoBasic/>
-        </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return (
+//         <React.Fragment>
+//           {/*<StateSample/>*/}
+//           {/*<InputSample/>*/}
+//           {/*<StateCounter/>*/}
+//           {/*<HigherLower/>*/}
+//           {/*<Converter/>*/}
+//           {/*<HookSample/>*/}
+//           {/*<StateCounterHooks/>*/}
+//           {/*<ConverterHooks/>*/}
+//           {/*<HigherLower/>*/}
+//           {/*<HigherLowerHooks/>*/}
+//           {/*<ListSample/>*/}
+//           <TodoBasic/>
+//         </React.Fragment>
+//     );
+//   }
+// }
+
+export function App() {
+  return (
+    <div>
+      <h1>App</h1>
+    </div>
+  );
 }
 
-export default App;
+export function Home() {
+  return (
+    <div>
+      <h1>ACME consulting</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur magna sapien, sit amet gravida ex ultrices a. Sed interdum justo ac erat pulvinar mattis. Phasellus bibendum lorem lectus, id posuere neque sodales nec. Sed semper tristique sodales. Curabitur neque purus, vestibulum non imperdiet eget, cursus sit amet turpis. Aliquam at placerat libero. Quisque et porttitor velit. Morbi posuere fermentum molestie. Aliquam efficitur nibh eget ipsum egestas porta. Curabitur sem massa, fermentum nec purus et, maximus lacinia velit. Integer at ipsum pulvinar, placerat nisl quis, finibus justo. Nulla at blandit risus, id sodales sapien. Proin rutrum eu risus vitae varius. In sit amet nulla eget mi pharetra commodo a at purus.</p>
+    </div>
+  )
+}
+
+export function About() {
+  return (
+    <div>
+      <h1>About ACME</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur magna sapien, sit amet gravida ex ultrices a. Sed interdum justo ac erat pulvinar mattis. Phasellus bibendum lorem lectus, id posuere neque sodales nec. Sed semper tristique sodales. Curabitur neque purus, vestibulum non imperdiet eget, cursus sit amet turpis. Aliquam at placerat libero. Quisque et porttitor velit. Morbi posuere fermentum molestie. Aliquam efficitur nibh eget ipsum egestas porta. Curabitur sem massa, fermentum nec purus et, maximus lacinia velit. Integer at ipsum pulvinar, placerat nisl quis, finibus justo. Nulla at blandit risus, id sodales sapien. Proin rutrum eu risus vitae varius. In sit amet nulla eget mi pharetra commodo a at purus.</p>
+    </div>
+  )
+}
+
+export function Contact() {
+  return (
+    <div>
+      <h1>Contact us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam efficitur magna sapien, sit amet gravida ex ultrices a. Sed interdum justo ac erat pulvinar mattis. Phasellus bibendum lorem lectus, id posuere neque sodales nec. Sed semper tristique sodales. Curabitur neque purus, vestibulum non imperdiet eget, cursus sit amet turpis. Aliquam at placerat libero. Quisque et porttitor velit. Morbi posuere fermentum molestie. Aliquam efficitur nibh eget ipsum egestas porta. Curabitur sem massa, fermentum nec purus et, maximus lacinia velit. Integer at ipsum pulvinar, placerat nisl quis, finibus justo. Nulla at blandit risus, id sodales sapien. Proin rutrum eu risus vitae varius. In sit amet nulla eget mi pharetra commodo a at purus.</p>
+    </div>
+  )
+}
+
+// export default App;
--- temp/hello_react/src/index.js
+++ temp/hello_react/src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import {About, App, Contact} from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="MyApp">
+    <BrowserRouter>
+      <h1>Home Page</h1>
+
+      <nav className="navbar">
+        <NavLink to="/">home</NavLink>
+        <NavLink to="/about">about us</NavLink>
+        <NavLink to="/contact">contact us</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<App/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      </Routes>
+    </BrowserRouter>
+  </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
